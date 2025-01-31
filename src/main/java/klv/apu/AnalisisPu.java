4a1f8eb774dcf89333c4360868cf414205dc6089
Luis Mujica <lmujica1010@gmail.com>
2025-01-31 15:07:57 -0300

    update

--- src/main/java/klv/apu/AnalisisPu.java
+++ src/main/java/klv/apu/AnalisisPu.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AnalisisPu {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AnalisisPu.class, args);
 	}
 
